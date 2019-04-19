175b3006b4fdf32708ecad4d15e0274d0b9f5204
Lucas Seinfeld <l.seinfeld@ixolit.com>
2019-04-20 01:57:19 +0200

    fix cs

--- bootstrap/servicemanager.php
+++ bootstrap/servicemanager.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * @link https://github.com/ixocreate
+ * @copyright IXOCREATE GmbH
+ * @license MIT License
+ */
+
 declare(strict_types=1);
 
 namespace Ixocreate\Filter;
@@ -6,5 +12,4 @@ namespace Ixocreate\Filter;
 use Ixocreate\Application\Service\ServiceManagerConfigurator;
 
 /** @var ServiceManagerConfigurator $serviceManager */
-
 $serviceManager->addFactory(Filter::class);
